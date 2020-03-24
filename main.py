db064bf59a3a920a1860956140554b704c4b088a
craig fay <craigfay1@gmail.com>
2020-03-24 01:07:25 -0500

    Update python version, create plot

--- main.py
+++ main.py
@@ -9,8 +9,23 @@ def mandelbrot(Re, Im, max_iter):
 
     for i in range(max_iter):
         z = z*z + c
-        if (z.real * z.real + z.image * z.imag) >= 4:
+        if (z.real * z.real + z.imag * z.imag) >= 4:
             return i
 
     return max_iter;
+
+columns = 2000
+rows = 2000
+
+result = numpy.zeros([rows, columns])
+
+for row_index, Re, in enumerate(numpy.linspace(-2, 1, num=rows)):
+    for column_index, Im, in enumerate(numpy.linspace(-1, 1, num=columns)):
+        result[row_index, column_index] = mandelbrot(Re, Im, 100)
+
+plt.figure(dpi=100)
+plt.imshow(result.T, cmap="hot", interpolation='bilinear', extent=[-2, 1, -1, 1])
+plt.xlabel('Re')
+plt.ylabel('Im')
+plt.show()
 
